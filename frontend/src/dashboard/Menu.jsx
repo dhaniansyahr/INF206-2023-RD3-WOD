d1554c1000985425e4c39ccd931bc4695026450b
raishabirah <raihanshabirah20@gmail.com>
2023-04-16 00:04:32 +0700

    feat: Add dashboard

---
+++ frontend/src/dashboard/Menu.jsx
@@ -0,0 +1,61 @@
+export default function Menu() {
+  return (
+    <ul className=" mt-[10px] align-center ml-[10px]">
+      <li className="relative list-none h-[50px] ">
+        <a
+          className="bg-transparent h-full w-full flex items-center decoretion-none transition-all duration-500 ease-linear hover:text-[#081D45] active"
+          href="/"
+        >
+          <i className="min-w-[60px] align-center text-18px text-[#ffff]"></i>
+          <h2 className="text-[#fff] text-[12px] font-light hover:text-[#00C78A] whitespace-nowrap">
+            Pemesanan
+          </h2>
+        </a>
+      </li>
+      <li className="relative list-none h-[50px] ">
+        <a
+          className="bg-transparent h-full w-full flex items-center decoretion-none transition-all duration-500 ease-linear hover:text-[#081D45] active"
+          href="/"
+        >
+          <i className="min-w-[60px] align-center text-18px text-[#ffff]"></i>
+          <h2 className="text-[#fff] text-[12px] font-light hover:text-[#00C78A] whitespace-nowrap">
+            Promo
+          </h2>
+        </a>
+      </li>
+      <li className="relative list-none h-[50px] ">
+        <a
+          className="bg-transparent h-full w-full flex items-center decoretion-none transition-all duration-500 ease-linear hover:text-[#081D45] active"
+          href="/"
+        >
+          <i className="min-w-[60px] align-center text-18px text-[#ffff]"></i>
+          <h2 className="text-[#fff] text-[12px] font-light hover:text-[#00C78A] whitespace-nowrap">
+            Akun Saya
+          </h2>
+        </a>
+      </li>
+      <li className="relative list-none h-[50px] ">
+        <a
+          className="bg-transparent h-full w-full flex items-center decoretion-none transition-all duration-500 ease-linear hover:text-[#081D45] active"
+          href="/"
+        >
+          <i className="min-w-[60px] align-center text-18px text-[#ffff]"></i>
+          <h2 className="text-[#fff] text-[12px] font-light hover:text-[#00C78A] whitespace-nowrap">
+            Informasi Pemesanan
+          </h2>
+        </a>
+      </li>
+      <li className="relative list-none h-[50px] ">
+        <a
+          className="bg-transparent h-full w-full flex items-center decoretion-none transition-all duration-500 ease-linear hover:text-[#081D45] active"
+          href="/"
+        >
+          <i className="min-w-[60px] align-center text-18px text-[#ffff]"></i>
+          <h2 className="text-[#fff] text-[12px] font-light hover:text-[#00C78A] whitespace-nowrap">
+            Pembayaran
+          </h2>
+        </a>
+      </li>
+    </ul>
+  );
+}
