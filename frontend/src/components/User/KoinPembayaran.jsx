2d27d35044fd804cbf5a4b06d92ae363f63a63c2
raishabirah <raihanshabirah20@gmail.com>
2023-04-29 18:34:30 +0700

    feat: add component page pembayaran

---
+++ frontend/src/components/User/KoinPembayaran.jsx
@@ -0,0 +1,11 @@
+export default function Koin() {
+    return (
+      <div class="md:px-[56px] p-5">
+        <div class=" bg-emerald-800 p-[32px] rounded-lg relative">
+          <h2 class="text-2xl font-bold mb-2 text-black font-poppins">
+            W - Coin : 1000W
+          </h2>
+        </div>
+      </div>
+    );
+  }
