73ac4413b6a6c828df679793b5b8dae34a7ed9f2
raishabirah <raihanshabirah20@gmail.com>
2023-04-29 18:42:10 +0700

    feat: add context data pembayaran

---
+++ frontend/src/context/DataPembayaranContext.js
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from "react";
+
+export const DataPembayaranContext = createContext();
+
+export const dataPembayaranReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_DATAPEMBAYARAN':
+            return {
+                dataPembayaran: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export const DataPembayaranContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(dataPembayaranReducer, {
+        dataPembayaran: null
+    })
+
+    return (
+        <DataPembayaranContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </DataPembayaranContext.Provider>
+    )
+}
