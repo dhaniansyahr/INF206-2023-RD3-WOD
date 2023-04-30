20a5c96cb0cd9d406cedcc978fe4f27199fc15c3
Riyadhusshadiqin <127411388+riyadhussss@users.noreply.github.com>
2023-04-30 15:41:50 +0700

    menambahkan Informasi Pemesanan

--- frontend/src/pages/UserPage/InformasiPemesanan.jsx
+++ frontend/src/pages/UserPage/InformasiPemesanan.jsx
@@ -1,14 +1,44 @@
 import Sidebar from "../../components/Sidebar";
-import CardInformasi from "../../components/User/ComponentInformasi.";
+
+import DataInformasiPemesanan from "../../components/User/DataInformasiPemesanan";
+
+import { useEffect } from "react";
+import { useDataPemesanan } from "../../hooks/useDataPemesanan";
 
 const Informasi = () => {
+  const { dataPemesanan, dispatch } = useDataPemesanan();
+
+  // Fungsi Untuk mem Fetching datas dari DB ke FE
+  useEffect(() => {
+    const fetchData = async () => {
+      // Variable Response Untuk menampung datas dari localhost BE
+      const response = await fetch("/api/wod/pemesanan");
+      // Kemudian di Ubah menjadi Data JSON
+      const json = await response.json();
+
+      // Jika REsponse OK maka datas akan di set
+      if (response.ok) {
+        dispatch({ type: "SET_DATAPEMESANAN", payload: json });
+      }
+    };
+
+    // Call FUnction Fetch Data
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="flex flex-row bg-[#fff]">
       <div className="md:basis-1/5 ">
         <Sidebar />
       </div>
-      <div className="md:basis-4/5 flex justify-center pr-[50px] md:pl-[45px]">
-        <CardInformasi />
+      <div className="w-screen h-screen">
+        <div className="flex flex-col gap-20 items-center justify-center p-10">
+          {dataPemesanan &&
+            dataPemesanan.map((data) => (
+              <DataInformasiPemesanan key={data._id} data={data} />
+            ))}
+        </div>
       </div>
     </div>
   );
