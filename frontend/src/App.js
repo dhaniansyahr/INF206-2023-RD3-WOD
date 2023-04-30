82016075d06c0187a9a1187559572f508d70a52f
Rama Dhaniansyah <71095453+Meone11@users.noreply.github.com>
2023-05-01 01:12:04 +0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { WavyContainer } from 'react-wavy-transitions';
 
 // Import Pages and Component
 // import Home from './pages/Home';
@@ -8,30 +9,39 @@ import HelpPage from './pages/LandingPage/HelpPage'
 import AboutUsPage from './pages/LandingPage/AboutUsPage'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
-import Home from './pages/Home';
+import Pemesanan from './pages/UserPage/Pemesanan';
+import AkunSaya from './pages/UserPage/AkunSaya';
+import InformasiPemesanan from './pages/UserPage/InformasiPemesanan';
+import Pembayaran from './pages/UserPage/Pembayaran';
+import RincianPembayaran from './pages/UserPage/RincianPembayaran';
 
 function App() {
   return (
     <div className='bg-white font-sans'>
       <BrowserRouter>
-        <div className='max-w-screen'>
-          <Routes>
-            <Route
-              // Unutk Path nya 
-              path='/'
-              // Untuk akses Component Pages
-              element={<HomePage />}
-            />
-            <Route path='/home' element={<HomePage />} />
-            <Route path='/partnerwithus' element={<PartnerWithUsPage />} />
-            <Route path='/help' element={<HelpPage />} />
-            <Route path='/aboutus' element={<AboutUsPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/Signup' element={<SignUpPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
+        <WavyContainer />
+        <Routes>
+          <Route
+            // Unutk Path nya 
+            path='/'
+            // Untuk akses Component Pages
+            element={<HomePage />}
+          />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/partnerwithus' element={<PartnerWithUsPage />} />
+          <Route path='/help' element={<HelpPage />} />
+          <Route path='/aboutus' element={<AboutUsPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/Signup' element={<SignUpPage />} />
+          <Route path='/pemesanan' element={<Pemesanan />} />
+          <Route path='/akun-saya' element={<AkunSaya />} />
+          <Route path='/informasi-pemesanan' element={<InformasiPemesanan />} />
+          <Route path='/pembayaran' element={<Pembayaran />} />
+          <Route path='/rincian-pembayaran' element={<RincianPembayaran />} />
+
+        </Routes>
+      </BrowserRouter >
+    </div >
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HelpDataContextProvider } from './context/HelpDataContext';
+import { DataPemesananContextProvider } from './context/DataPemesananContext';
 import { AuthContextProvider } from './context/AuthContext';
+import { DataPembayaranContextProvider } from './context/DataPembayaranContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <HelpDataContextProvider>
-        <App />
+        <DataPemesananContextProvider>
+          <DataPembayaranContextProvider>
+            <App />
+          </DataPembayaranContextProvider>
+        </DataPemesananContextProvider>
       </HelpDataContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
