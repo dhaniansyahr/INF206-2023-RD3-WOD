f280ee1bf2e18f2e10151b6aa8ff2126965e88e2
Riyadhusshadiqin <127411388+riyadhussss@users.noreply.github.com>
2023-05-11 22:56:20 +0700

    feat : add DataInformasiPemesanan.jsx

---
+++ frontend/src/components/Driver/DataInformasiPemesanan.jsx
@@ -0,0 +1,12 @@
+const DataInformasiPemesanan = () => {
+  return (
+    <div class="bg-white w-full h-fit p-10 drop-shadow-lg rounded-2xl">
+      <h2 class="text-2xl font-bold mb-2 text-black">Dahlia</h2>
+      <p class="text-black mb-[8px]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+        suscipit?
+      </p>
+    </div>
+  );
+};
+export default DataInformasiPemesanan;
